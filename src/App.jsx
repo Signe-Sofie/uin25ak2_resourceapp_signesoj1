4ec4333313e81a0ee6c345e36ffbc2772a173421
Signe-Sofie <142736190+Signe-Sofie@users.noreply.github.com>
2025-03-13 20:54:15 +0100

    små endringer

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,10 @@ import Layout from './components/Layout'
 import { Route, Routes } from 'react-router-dom' 
 import Resources from './components/Resources' 
 
-
+/*Jeg har fått hjelp til å løse oppgaven fra Hanna Hänel Sørum, etter å ha snakket med Marius så fikk vi besjed om at det går fint så lenge 
+jeg er åpen om det å skriver en kommentar i koden. 
+Kjære Hanna, hva skulle jeg gjort uten deg, du er en god venn, en støttespiller og min store glede her på skolen. Jeg setter pris på deg,
+alt du gjør for meg og alt vi gjør sammen, det er en sann glede å ha blitt kjent med deg. Utrolig glad i deg, hilsen din lille sidekick*/
 function App() {
   const [count, setCount] = useState(0)
 
